6f6a73a1e07770babfe30a0e7fb28eceecbf287d
DevGandara <david.gandara19@gmail.com>
2026-08-26 07:43:08 -0600

    feaf(parcial01): add first entity and initial migration

--- src/Persistence/AppDbContext.cs
+++ src/Persistence/AppDbContext.cs
@@ -5,6 +5,6 @@ namespace Persistence
 {
     public class AppDbContext(DbContextOptions options) : DbContext(options)
     {
-        public DbSet<Activity> Activities { get; set; } // el get; set; hace automaticamente el getter y el setter
+        public DbSet<Event> Events { get; set; } // el get; set; hace automaticamente el getter y el setter
     }
 }
