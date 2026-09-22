d2f6829932797f9c8e7d4dcfdeeb961bb7831c24
DevGandara <david.gandara19@gmail.com>
2026-09-22 07:46:23 -0600

    feat(parcial02): remove primary constructor

--- src/EventsHub.API/Controllers/EventsController.cs
+++ src/EventsHub.API/Controllers/EventsController.cs
@@ -1,24 +1,21 @@
 using Domain;
 using Microsoft.AspNetCore.Mvc;
-using Persistence;
-using Microsoft.EntityFrameworkCore;
-using MediatR;
 using EventsHub.Application.Events.Queries;
 
 namespace EventsHub.API.Controllers
 {
-    public class EventsController(IMediator mediator) : EventsHubBaseController
+    public class EventsController : EventsHubBaseController
     {
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<Event>>> GetEventsAsync()
         {
-            return await mediator.Send(new GetEventList.Query());
+            return await Mediator.Send(new GetEventList.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Event>> GetEventByIdAsync(string id)
         {
-            return await mediator.Send(new GetEventDetails.Query{Id = id});
+            return await Mediator.Send(new GetEventDetails.Query{Id = id});
         }
     }
 }
--- src/EventsHub.API/Controllers/EventsHubBaseController.cs
+++ src/EventsHub.API/Controllers/EventsHubBaseController.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EventsHub.API.Controllers;
@@ -5,5 +6,9 @@ namespace EventsHub.API.Controllers;
 [ApiController]
 public class EventsHubBaseController : ControllerBase 
 {
-    
+    private IMediator? _mediator;
+
+    protected IMediator Mediator =>
+        _mediator ??= HttpContext.RequestServices.GetService<IMediator> ()
+            ?? throw new InvalidOperationException("IMediator service is unavailable");
 }
