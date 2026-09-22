c1576d8d9b4c875b8d9034114831cc9220ca132f
DevGandara <david.gandara19@gmail.com>
2026-09-22 07:26:02 -0600

    feat(parcial02): add  mediatr handler

--- src/EventsHub.API/Controllers/EventsController.cs
+++ src/EventsHub.API/Controllers/EventsController.cs
@@ -2,28 +2,23 @@ using Domain;
 using Microsoft.AspNetCore.Mvc;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
+using MediatR;
+using EventsHub.Application.Events.Queries;
 
 namespace EventsHub.API.Controllers
 {
-    public class EventsController(AppDbContext context) : EventsHubBaseController
+    public class EventsController(IMediator mediator) : EventsHubBaseController
     {
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<Event>>> GetEventsAsync()
         {
-            return await context.Events.ToListAsync();
+            return await mediator.Send(new GetEventList.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Event>> GetEventByIdAsync(string id)
         {
-            var @event = await context.Events.FindAsync(id);
-
-            if (@event == null)
-            {
-                return NotFound("The event was not found.");
-            }
-
-            return @event;
+            return await mediator.Send(new GetEventDetails.Query{Id = id});
         }
     }
 }
--- src/EventsHub.API/Program.cs
+++ src/EventsHub.API/Program.cs
@@ -1,3 +1,4 @@
+using EventsHub.Application.Events.Queries;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -11,6 +12,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 });
 
 builder.Services.AddCors();
+builder.Services.AddMediatR(opt => opt.RegisterServicesFromAssemblyContaining<GetEventList.Handler>());
 
 var app = builder.Build();
 
